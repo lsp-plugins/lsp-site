c62ac364c332a6ee15e1054a1a15712c4106d5c8
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-09-17 13:02:46 +0300

    Updated feedback form

--- pages/mail/submit.php
+++ pages/mail/submit.php
@@ -5,34 +5,6 @@
 
 
 <form id="fb_form" action="<?=$SITEROOT?>/mail.php?send=1" method="POST">
-	<!--<table>
-		<tr>
-			<td>Your name:</td>
-			<td><input type="text" name="name"></td>
-		</tr>
-		<tr>
-			<td>Contact e-mail:</td>
-			<td><input type="text" name="email"></td>
-		</tr>
-		<tr>
-			<td colspan="2">Enter the message you want to inform us:</td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				<textarea name="text"></textarea>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				<div data-theme="dark" class="g-recaptcha" data-sitekey="<?= $GOOGLE['recaptcha_pub'] ?>"></div>
-			</td>
-		</tr>
-		<tr>
-			<td colspan="2">
-				<input type="submit" value="Send feedback">
-			</td>
-		</tr>
-	</table>-->
 	<div class="feedback-submit">
 		<div class="fs-name">Your name:<input type="text" name="name"></div>
 		<div class="fs-email">Contact e-mail:<input type="text" name="email"></div>
