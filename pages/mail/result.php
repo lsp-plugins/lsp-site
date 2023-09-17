15ddbb2b944dfb74f262f71b8673b72c33b641b3
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-09-17 13:14:35 +0300

    Updated feedback

--- pages/mail/result.php
+++ pages/mail/result.php
@@ -1,7 +1,14 @@
+<div style="display:inline-block; width:100%;">
+<p style="text-align:center;">If you want to contact to developers, please submit the following form.</p>
+<div class="fb-div">
+
 <form id="fb_form" action="<?=$SITEROOT?>/?page=feedback" method="GET">
 	<div>
 		<p><?php echo $message ?></p>
 		<input type="button" value="<?php echo $button ?>" onclick="location.reload();">
 	</div>
 </form>
+
+</div>
+</div>
 
