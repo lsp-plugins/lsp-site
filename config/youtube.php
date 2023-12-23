1e2179d16b98f6c41e2737e88b1f3429fc6dbf7a
Vladimir Sadovnikov <sadko4u@gmail.com>
2023-12-23 16:50:02 +0300

    Updated YouTube links

--- config/youtube.php
+++ config/youtube.php
@@ -26,11 +26,13 @@ $YOUTUBE = array(
 	'6ZuW28u1qns' => 'Multiband Dynamic Processor',
 	'1Og6vAZ2BLo' => 'Noise Generator',
 	'EFf4VqvMUXM' => 'A/B Tester & Mixer',
-	'BN0h4WnNnW8' => 'Filter',
-	'_WD9GndORQA' => 'Flanger',
 	'wb8QnU4yRF0' => 'GOTT Compressor',
 	'_0VjhooWRBQ' => 'Multiband Limiter',
-	'-mJ0fQqwAy4' => 'Beat Breather'
+	'BN0h4WnNnW8' => 'Filter',
+	'_WD9GndORQA' => 'Flanger',
+	'-mJ0fQqwAy4' => 'Beat Breather',
+	'i_10kAtZmJU' => 'Autogain',
+	'jbnGSgoVIkg' => 'Clipper & Multiband Clipper'
 );
 
 ?>
