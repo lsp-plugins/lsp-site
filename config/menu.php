017c3f52459fb3749a1b999f3abbb263655dbf5c
sadko4u <sadko4u@gmail.com>
2023-12-30 18:14:30 +0000

    Updated documentation

--- config/menu.php
+++ config/menu.php
@@ -20,6 +20,7 @@
 	   def_menu_item('development', 'building', 'Building');
 	   def_menu_item('development', 'testing', 'Testing');
 	   def_menu_item('development', 'eclipse', 'Eclipse IDE Integration');
+	   def_menu_item('development', 'ui_guidelines', 'UI Development Guidelines');
 	def_menu_item('', 'troubleshooting', 'Troubleshooting');
 	
 ?>
