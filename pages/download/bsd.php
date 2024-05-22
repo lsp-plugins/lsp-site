2b587c09da02340780229d9a6a5d89a437ce77f3
sadko4u <sadko4u@gmail.com>
2024-05-22 19:05:01 +0000

    Updates from Bor

--- pages/download/bsd.php
+++ pages/download/bsd.php
@@ -7,17 +7,17 @@ ksort($artifacts);
 
 ?>
 
-<h2>Latest Release</h2>
+<h2>Latest release for FreeBSD</h2>
 
 <p>The latest release version for FreeBSD is <?= htmlspecialchars($latest_version); ?>.</p>
 <p>Because FreeBSD is a libre platform, you can freely download plugin builds for this platform without any charge.</p>
 <p>All possible builds are listed in the table below.</p>
 
-<table>
-<tr>
-	<th>Architecture</th>
-	<th>Package</th>
-	<th>Files</th>
+<table class="dwnld-tbl">
+<tr class="dwnld-tbl-tr">
+	<th class="dwnld-tbl-th" >Architecture</th>
+	<th class="dwnld-tbl-th" >Package</th>
+	<th class="dwnld-tbl-th" >Files</th>
 </tr>
 
 <?php
@@ -25,17 +25,17 @@ $base_url = "{$CODE_REPO}/releases/download/{$latest_version}/";
 
 foreach ($artifacts as $architecture => $artifacts) {
 	$arch = htmlspecialchars($architecture);
-	echo "<tr>\n";
-	echo "<td>{$arch}</td>\n";
-	echo "<td>lsp-plugins</td>\n";
-	echo "<td>\n";
+	echo "<tr class=\"dwnld-tbl-tr\">\n";
+	echo "<td class=\"dwnld-tbl-td\">{$arch}</td>\n";
+	echo "<td class=\"dwnld-tbl-td\">lsp-plugins</td>\n";
+	echo "<td class=\"dwnld-tbl-td\">\n";
 
 	foreach ($artifacts as $artifact) {
 		$url = htmlspecialchars($base_url . $artifact['file']);
 		$fmt = htmlspecialchars($artifact['format']);
 		$alt = htmlspecialchars($format_names[$artifact['format']]);
-		
-		echo "<a href=\"{$url}\" class=\"formats-links {$fmt}\" alt=\"{$alt}\"></a>\n";
+
+		echo "<a href=\"{$url}\" class=\"formats-links {$fmt}-dwnld\" alt=\"{$alt}\"></a>\n";
 	}
 
 	echo "</td>\n";
@@ -54,4 +54,3 @@ foreach ($artifacts as $architecture => $artifacts) {
 		<img alt="Browse archive builds" src="https://a.fsdn.com/con/app/sf-download-button">
 	</a>
 </p>
-
--- pages/download/linux.php
+++ pages/download/linux.php
@@ -7,16 +7,16 @@ ksort($artifacts);
 
 ?>
 
-<h2>Latest Release</h2>
+<h2>Latest release for Linux</h2>
 
 <p>The latest release version for GNU/Linux is <?= htmlspecialchars($latest_version); ?>.</p>
 <p>Because Linux is a libre platform, you can freely download plugin builds for this platform without any charge.</p>
 
-<table>
-<tr>
-	<th>Architecture</th>
-	<th>Package</th>
-	<th>Files</th>
+<table class="dwnld-tbl">
+<tr class="dwnld-tbl-tr">
+	<th class="dwnld-tbl-th" >Architecture</th>
+	<th class="dwnld-tbl-th" >Package</th>
+	<th class="dwnld-tbl-th" >Files</th>
 </tr>
 
 <?php
@@ -24,17 +24,17 @@ $base_url = "{$CODE_REPO}/releases/download/{$latest_version}/";
 
 foreach ($artifacts as $architecture => $artifacts) {
 	$arch = htmlspecialchars($architecture);
-	echo "<tr>\n";
-	echo "<td>{$arch}</td>\n";
-	echo "<td>lsp-plugins</td>\n";
-	echo "<td>\n";
+	echo "<tr class=\"dwnld-tbl-tr\">\n";
+	echo "<td class=\"dwnld-tbl-td\">{$arch}</td>\n";
+	echo "<td class=\"dwnld-tbl-td\">lsp-plugins</td>\n";
+	echo "<td class=\"dwnld-tbl-td\">\n";
 
 	foreach ($artifacts as $artifact) {
 		$url = htmlspecialchars($base_url . $artifact['file']);
 		$fmt = htmlspecialchars($artifact['format']);
 		$alt = htmlspecialchars($format_names[$artifact['format']]);
-		
-		echo "<a href=\"{$url}\" class=\"formats-links {$fmt}\" alt=\"{$alt}\"></a>\n";
+
+		echo "<a href=\"{$url}\" class=\"formats-links {$fmt}-dwnld\" alt=\"{$alt}\"></a>\n";
 	}
 
 	echo "</td>\n";
@@ -53,4 +53,3 @@ foreach ($artifacts as $architecture => $artifacts) {
 		<img alt="Browse archive builds" src="https://a.fsdn.com/con/app/sf-download-button">
 	</a>
 </p>
-
