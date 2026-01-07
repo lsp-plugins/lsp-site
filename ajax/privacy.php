022726f2ce04e32e1b725f89e73c58d4a6a8475c
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-01-07 18:34:22 +0300

    Updated AJAX

--- ajax/privacy.php
+++ ajax/privacy.php
@@ -1,7 +1,7 @@
 <?php
 chdir($_SERVER['DOCUMENT_ROOT']);
 require_once("./inc/top.php");
-echo '<div class="main smooth"';
+echo '<div class="main smooth">';
 require_once("./pages/privacy.php");
 echo '</div>'
 ?>
