0de13916ec74614d56b7ccac44e35b4e57baf8dc
bor-T <bor-tattoo@ya.ru>
2026-01-27 18:21:13 +0200

    The cart design with the number of items has been created. The buttons have been redesigned.

--- pages/download/parts/cart.php
+++ pages/download/parts/cart.php
@@ -17,21 +17,37 @@ function show_user_cart($user_cart, $user_purchases) {
 	}
 
 	$total = sprintf("%.2f", raw_to_price($total));
-	$items = ($items === 1) ? "{$items} item" : "{$items} items";
+	$items_text = ($items === 1) ? "{$items} item" : "{$items} items";
+
 	echo "<div id=\"user-cart\" class=\"user-cart\">\n";
-	echo "<div>Cart: {$total} USD ({$items})</div>\n";
 	echo "<div>\n";
 	if ($total > 0) {
 		$csrf_token = make_csrf_token('cart');
 
 		echo "<div>\n";
-		echo "<form action=\"{$SITEROOT}/actions/checkout\" method=\"POST\">\n";
+		echo "<form class=\"user-cart-buttons\" action=\"{$SITEROOT}/actions/checkout\" method=\"POST\">\n";
 		echo "<input type=\"hidden\" value=\"{$csrf_token}\">\n";
-		echo "<input type=\"submit\" value=\"Checkout\" name=\"checkout\">\n";
-		echo "<input type=\"button\" value=\"Empty\" name=\"empty\" onclick=\"javascript:ajax_post('empty_cart', { 'token': '{$csrf_token}' });\">\n";
+		echo "<input class=\"cart-button user-cart-button-empty\" type=\"button\" value=\"Empty\" name=\"empty\" onclick=\"javascript:ajax_post('empty_cart', { 'token': '{$csrf_token}' });\">\n";
+		echo "<div class=\"user-cart-button-checkout-wrapper\">\n";
+		echo "<input class=\"cart-button user-cart-button-checkout\" type=\"submit\" value=\"Checkout\" name=\"checkout\">\n";
+		echo "<div class=\"user-cart-items\">{$items}</div>\n";
+		echo "</div>\n";
 		echo "</form>\n";
 		echo "</div>\n";
 	}
+	else {
+			echo "<div>\n";
+		echo "<div class=\"user-cart-buttons\">\n";
+		echo "<div class=\"cart-button user-cart-button-empty cart-inactive\">Empty</div>\n";
+		echo "<div class=\"user-cart-button-checkout-wrapper\">\n";
+		echo "<div class=\"cart-button user-cart-button-checkout cart-inactive\">Checkout</div>\n";
+		echo "<div class=\"user-cart-items-zero\">0</div>\n";
+		echo "</div>\n";
+		echo "</div>\n";
+		echo "</div>\n";
+	}
+
+	echo "<div class=\"user-cart-total\">Cart: {$total} USD ({$items_text})</div>\n";
 	echo "</div>\n";
 	echo "</div>\n";
 }
