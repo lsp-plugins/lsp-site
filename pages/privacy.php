e354636a479a8f2ad00e22a8321dc6a8b429e36b
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-03-26 09:33:11 +0300

    Updated privacy

--- pages/privacy.php
+++ pages/privacy.php
@@ -4,12 +4,14 @@
 
 <h2>Preamble</h2>
 
-<p><b>Last updated</b>: 2026-01-07.</p>
+<p><b>Last updated</b>: 2026-03-26.</p>
 
-<p>We are committed to protecting your privacy and ensuring the security of your personal data. This Privacy Policy outlines how we collect, use, and protect your information when you use our website.</p>
+<p>We are committed to protecting your privacy and ensuring the security of your personal data. This Privacy Policy outlines
+how we collect, use, and protect your information when you use our website.</p>
 
 <h2>1. Use of Cookies</h2>
-<p>Our website uses cookies to identify and maintain your user session. These cookies are essential for providing a secure and personalized experience while you navigate our site.</p>
+<p>Our website uses cookies to identify and maintain your user session. These cookies are essential for providing a secure
+and personalized experience while you navigate our site.</p>
 
 <h2>2. Collection of Personal Information</h2>
 <p>We may collect the following personal information from you:</p>
@@ -30,8 +32,16 @@
 <p>All personal data that is stored for an extended period is encrypted using a strong and reliable encryption algorithm.
 We take all necessary technical and organizational measures to protect your data from unauthorized access, loss, or misuse.</p>
 
-<h2>5. Your Consent</h2>
-<p>By using our website, you agree to the collection, use, and protection of your personal data as described in this Privacy Policy.</p>
+<h2>5. Support Identifier and Data Depersonalization</h2>
+<p>To further enhance your privacy and minimize the use of personal information during technical interactions,
+each registered user is assigned a unique <b>'Support Identifier'</b>. This code is used to facilitate communication with our
+support team. It allows us to identify your account status and technical logs in a depersonalized manner, ensuring that
+our staff can provide effective assistance without needing to access your direct personal identity or contact details
+unless absolutely necessary.</p>
+
+<h2>6. Your Consent</h2>
+<p>By using our website, you agree to the collection, use, and protection of your personal data as described in this
+Privacy Policy.</p>
 
 <p>If you have any questions or concerns regarding this policy, please feel free to contact us.</p>
 
