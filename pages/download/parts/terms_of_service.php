92fa9b3fb6b19551618626a9811f98e2ff301323
Vladimir Sadovnikov <sadko4u@gmail.com>
2026-03-26 09:03:05 +0300

    Updated link

--- pages/download/parts/terms_of_service.php
+++ pages/download/parts/terms_of_service.php
@@ -9,5 +9,5 @@
 <p>To provide personal download links of binary builds, we also need to collect some of your personal data.</p>
 
 <p>To get more information about collecting personal data, please read our
-<a data-fancybox data-type="ajax" data-src="/ajax/privacy_policy.php" href="javascript:;">Privacy Policy</a>
+<a data-fancybox data-type="ajax" data-src="/ajax/privacy.php" href="javascript:;">Privacy Policy</a>
 (<a href="/?page=privacy" alt="Direct link to Privacy Policy" target="_blank">Direct Link</a>).</p>
